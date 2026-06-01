183ac8329fea4540150f3fd2e4a2e35dae297310
b-tupiti <tupitibrandon@gmail.com>
2026-06-02 10:35:01 +1100

    feat(cms): configure Cloudflare R2 storage and Cloud Run deployment

--- cms/core/settings/production.py
+++ cms/core/settings/production.py
@@ -1,12 +1,52 @@
+import os
+
 from .base import *
 
 DEBUG = False
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+# Cloudflare R2 storage
+_R2_ENDPOINT = f"https://{os.environ.get('CLOUDFLARE_ACCOUNT_ID', '')}.r2.cloudflarestorage.com"
+_R2_BUCKET = os.environ.get("CLOUDFLARE_R2_BUCKET_NAME", "")
+_R2_KEY_ID = os.environ.get("CLOUDFLARE_R2_ACCESS_KEY_ID", "")
+_R2_SECRET = os.environ.get("CLOUDFLARE_R2_SECRET_ACCESS_KEY", "")
+_R2_PUBLIC_URL = os.environ.get("CLOUDFLARE_R2_PUBLIC_URL", "")  # e.g. https://assets.yourdomain.com
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "endpoint_url": _R2_ENDPOINT,
+            "bucket_name": _R2_BUCKET,
+            "access_key": _R2_KEY_ID,
+            "secret_key": _R2_SECRET,
+            "region_name": "auto",
+            "location": "media",
+            "file_overwrite": False,
+            "querystring_auth": False,
+            "custom_domain": _R2_PUBLIC_URL.removeprefix("https://") if _R2_PUBLIC_URL else None,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "endpoint_url": _R2_ENDPOINT,
+            "bucket_name": _R2_BUCKET,
+            "access_key": _R2_KEY_ID,
+            "secret_key": _R2_SECRET,
+            "region_name": "auto",
+            "location": "static",
+            "querystring_auth": False,
+            "custom_domain": _R2_PUBLIC_URL.removeprefix("https://") if _R2_PUBLIC_URL else None,
+        },
+    },
+}
+
+STATIC_URL = f"{_R2_PUBLIC_URL}/static/"
+MEDIA_URL = f"{_R2_PUBLIC_URL}/media/"
 
 try:
     from .local import *
