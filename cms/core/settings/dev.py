3bef8776e7defac82d9301437d25a28a73471f34
b-tupiti <tupitibrandon@gmail.com>
2026-06-02 08:59:35 +1100

    add cms

---
+++ cms/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b)om469m)umw9+1^8t^1#*)k7l^r%)34!z@7z4ke)l&18%i@7f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
